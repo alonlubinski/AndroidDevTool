616fe8332637347c4e0233129638330ee1ce9627
Alon Lubinski <alon.lubinski@gmail.com>
2021-01-06 18:33:09 +0200

    Initial commit

---
+++ AndroidDevTool/src/main/java/com/alon/androiddevtool/AndroidDevToolActivity.java
@@ -0,0 +1,117 @@
+package com.alon.androiddevtool;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+
+
+import com.google.android.material.navigation.NavigationView;
+
+public class AndroidDevToolActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private View navHeader;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+    private Fragment fragment = null;
+    private SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_dev_tool);
+        Log.d("pttt", "onCreate");
+
+        findAll();
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setupDrawerContent(navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
+        sharedPreferences = getApplicationContext().getSharedPreferences("sp", Context.MODE_PRIVATE);
+    }
+
+    // Find all views by id.
+    private void findAll() {
+        toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.main_DL);
+        navigationView = findViewById(R.id.main_NV);
+        navHeader = navigationView.getHeaderView(0);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectDrawerItem(item);
+                return true;
+            }
+        });
+    }
+
+    private void selectDrawerItem(MenuItem item) {
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_home){
+            Log.d("pttt", "Home");
+        } else if (item.getItemId() == R.id.menu_sp){
+            String value = sharedPreferences.getString("key", "empty");
+            Log.d("pttt", value);
+        }
+        if(fragment != null){
+            //changeFragment(fragment, item);
+        }
+        return false;
+    }
+
+    // Method that change the layout to the chosen fragment.
+    private void changeFragment(Fragment fragment, MenuItem item){
+        if(fragment != null){
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.main_FL, fragment).commit();
+            setTitle(item.getTitle());
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("pttt", "onPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("pttt", "onDestroy");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("pttt", "onStop");
+    }
+}
---
+++ app/src/main/java/com/alon/testmyandroiddevtool/MainActivity.java
@@ -0,0 +1,48 @@
+package com.alon.testmyandroiddevtool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button main_BTN_tool;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findAll();
+        setClickListeners();
+
+    }
+
+    private void findAll() {
+        main_BTN_tool = findViewById(R.id.main_BTN_tool);
+    }
+
+    private void setClickListeners() {
+        main_BTN_tool.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.main_BTN_tool:
+                startDevActivity();
+                break;
+
+        }
+    }
+
+    private void startDevActivity() {
+        Intent intent = new Intent("com.alon.androiddevtool");
+        startActivity(intent);
+    }
+}
+
